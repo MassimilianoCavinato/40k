326b9722f5a6693d1daf9ff78c8a22b4eac9bf0c
MassimilianoCavinato <massi304477@live.it>
2022-07-04 16:59:54 +0100

    first commit

---
+++ client_app/src/App.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three-full';
+import React from 'react';
+// import parser from 'rosz2js';
+// // console.log(parser)
+class App extends React.Component {
+ 
+  componentDidMount(){
+  //  this.test();
+  }
+
+  test(){
+    const scene = new THREE.Scene();
+    window.scene = scene;
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( 0xffffff );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById('root').appendChild( renderer.domElement );
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, .8 );
+    scene.add( directionalLight );
+    const light = new THREE.AmbientLight( 0x505050 ); // soft white light
+    scene.add( light );
+    camera.position.z = 5;
+    const controls = new THREE.OrbitControls( camera, renderer.domElement );
+    var textureLoader = new THREE.TextureLoader();
+    var map = textureLoader.load(require("./assets/canoness.jpeg"));
+    var material = new THREE.MeshPhongMaterial({map: map})
+    const loader = new THREE.OBJLoader();
+   
+    function animate() {
+        requestAnimationFrame( animate );
+        controls.update();
+        renderer.render( scene, camera );
+    }
+
+    loader.load(
+        require("./assets/canoness.obj"),
+        function ( object ) {
+            object.traverse( function ( node ) {
+                if ( node.isMesh ) node.material = material;
+            } );
+            scene.add( object );
+            
+        },
+        function ( xhr ) {
+        },
+        function ( error ) {
+            console.log( 'An error happened' );
+        }
+    );
+
+    animate();
+  }
+
+  render() {
+    return (
+      <div />
+    )
+  }
+
+}
+
+export default App;
